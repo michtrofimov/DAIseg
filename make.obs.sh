8d21f27f472774e8d4d32f1a422f9ed7d60a17e3
ekrez <84871963+ekrez@users.noreply.github.com>
2024-05-22 15:07:40 +0300

    Update make.obs.sh

--- make.obs.sh
+++ make.obs.sh
@@ -30,14 +30,14 @@ do
 paste $i.positions.txt $i.spec.txt  > chr${CHR}.$i.reference.txt
 done
 
-#for i in ${out} ${arch} 
-#do
-#rm $i.spec.*
-#rm $i.al.*
-#rm $i.0.*
-#rm $i.1.*
-#rm $i.positions.*
-#done
+for i in ${out} ${arch} 
+do
+rm $i.spec.*
+rm $i.al.*
+rm $i.0.*
+rm $i.1.*
+rm $i.positions.*
+done
 
 
 bcftools query -S ${eu}  -f '[%GT ]\n'  ${panelfinal} |sed  's/|/ /g' >  obs.chr${CHR}.ingroup.txt
@@ -45,15 +45,15 @@ bcftools query -S ${eu}  -f '[%GT ]\n'  ${panelfinal} |sed  's/|/ /g' >  obs.chr
 
 python3 obs.py ${CHR} chr${CHR}.${out}.reference.txt chr${CHR}.${arch}.reference.txt obs.chr${CHR}.ingroup.txt ${aa}
 
-#for i in ${out} ${arch} 
-#do
-#rm chr${CHR}.$i.reference.txt
-#done
-#rm obs.chr${CHR}.ingroup.txt
+for i in ${out} ${arch} 
+do
+rm chr${CHR}.$i.reference.txt
+done
+rm obs.chr${CHR}.ingroup.txt
 
 
 
-#rm positions.chr${CHR}.txt
+rm positions.chr${CHR}.txt
 
 
 
